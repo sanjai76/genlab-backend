cc57437fdb5c4777dfaabce8f9926063db1c7e51
Sanjai N <sanjaimsd141@gmail.com>
2026-03-24 13:47:47 +0530

    fix cors for production

--- backend/settings.py
+++ backend/settings.py
@@ -44,15 +44,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
@@ -130,3 +129,8 @@ EMAIL_HOST_PASSWORD = 'ajtv ovce nghh aalx'  # Your email password
 
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://genlab-frontend.vercel.app"
+]
